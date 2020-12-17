8f5b3bc313f332786d84498b307b468e51261d6c
Nirajan Dhakal <53692745+nirajandata@users.noreply.github.com>
2020-12-17 17:04:06 +0545

    Update script.py

--- script.py
+++ script.py
@@ -1,8 +1,13 @@
-a="netsh wlan show profile name=Nirajan001 key=clear" # it's cmd command to show saved password
 import subprocess
+
+a="netsh wlan show profile name=Nirajan001 key=clear" # it's cmd command to show saved password
 output = subprocess.check_output(a, shell=True) # os.system won't help us to save it in a variable
-f=open("myfile.txt", "x") #creating a file 
-f=open("myfile.txt", "a") #appending it
+myfile="wifi_pwd.txt"
+
+if path.isdir(myfile):
+ f=open(myfile, "x") #creating a file 
+
+f=open(myfile, "a")  #appending it
 output=str(output) # since f.write() wont work for byte 
 f.write(output)
 
